8820f2f2cb79cd541e59c5d504ff293c663fe88f
Joao Vitor <joaovlopes000@gmail.com>
2024-02-03 01:20:58 -0300

    closes #3

--- product-service/cmd/catalog/main.go
+++ product-service/cmd/catalog/main.go
@@ -2,15 +2,59 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
-func main() {
-	sql.Open("postgres", "host=lo")
+var db *sql.DB
 
+func main() {
+	connectDatabase()
+	createWebService()
+}
+
+func connectDatabase() {
+	cfg := getEnvVariable("DB_CONNECTION")
+
+	var err error
+	db, err = sql.Open("postgres", cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pingDatabase()
+	fmt.Println("Connected!")
+}
+
+func pingDatabase() {
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+}
+
+func createWebService() {
 	engine := gin.New()
 	engine.Use(gin.Recovery())
+	engine = defineRoutes(engine)
+	engine.Run(fmt.Sprintf(":%s", getEnvVariable("PORT")))
+}
 
-	engine.Run(":8080")
+func defineRoutes(engine *gin.Engine) *gin.Engine {
+	engine.GET("/")
+	return engine
+}
+
+func getEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
