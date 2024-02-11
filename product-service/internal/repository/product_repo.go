cb5bde84b9f0a7733a6015402282f2b7f9b0e8e5
Joao Vitor <joaovlopes000@gmail.com>
2024-02-11 16:03:27 -0300

    closes #6

--- product-service/internal/repository/product_repo.go
+++ product-service/internal/repository/product_repo.go
@@ -15,10 +15,76 @@ func NewProductRepo(db *sql.DB) *ProductRepo {
 }
 
 func (productRepo *ProductRepo) CreateProduct(product *domain.Product) (*domain.Product, error) {
-	_, err := productRepo.db.Exec("INSERT INTO products (id, name, description, image_url, price, quantity, department_id) VALUES (?, ?, ?, ?, ?, ?, ?)",
-		product.ID, product.Name, product.Description, product.ImageURL, product.Price, product.Quantity, product.DepartmentID)
+	sql := "INSERT INTO products (id, name, description, image_url, price, quantity, department_id) VALUES (?, ?, ?, ?, ?, ?, ?)"
+	_, err := productRepo.db.Exec(sql, product.ID, product.Name, product.Description, product.ImageURL, product.Price, product.Quantity, product.DepartmentID)
 	if err != nil {
 		return nil, err
 	}
 	return product, nil
+}
+
+func (productRepo *ProductRepo) FindProducts(page int8, pageSize int) ([]*domain.Product, error) {
+	offset := int(page-1) * pageSize
+	limit := pageSize
+
+	sql := "SELECT * FROM products LIMIT ? OFFSET ?"
+
+	rows, err := productRepo.db.Query(sql, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*domain.Product
+
+	for rows.Next() {
+		var product domain.Product
+		err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.Description)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
+func (productRepo *ProductRepo) FindProductById(id string) (*domain.Product, error) {
+	var product domain.Product
+	sql := "SELECT id, name, description, image_url, price, quantity, department_id FROM products WHERE id = ?"
+	err := productRepo.db.QueryRow(sql, id).
+		Scan(&product.ID, &product.Name, &product.Description, &product.ImageURL, &product.Price, &product.Quantity, &product.DepartmentID)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (productRepo *ProductRepo) FindProductByDepartmentId(departmentId string) ([]*domain.Product, error) {
+	sql := "SELECT id, name, description, image_url, price, quantity, department_id FROM products WHERE department_id = ?"
+	rows, err := productRepo.db.Query(sql, departmentId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*domain.Product
+	for rows.Next() {
+		var product domain.Product
+		err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.ImageURL, &product.Price, &product.Quantity, &product.DepartmentID)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return products, nil
 }
