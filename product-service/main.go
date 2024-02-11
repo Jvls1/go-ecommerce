afcca2c2ae6390f44a637203ff9cc78dc5e299fc
Joao Vitor <joaovlopes000@gmail.com>
2024-02-11 16:07:23 -0300

    closes #10

--- product-service/main.go
+++ product-service/main.go
@@ -6,13 +6,19 @@ import (
 	"log"
 	"os"
 
+	_ "github.com/Jvls1/go-ecommerce/product-service/docs"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-var db *sql.DB
-
+// @title           Product Service
+// @version         1.0
+// @host      			:8080
+// @BasePath  			/api/v1
 func main() {
 	connectDatabase()
 	createWebService()
@@ -22,15 +28,15 @@ func connectDatabase() {
 	cfg := getEnvVariable("DB_CONNECTION")
 
 	var err error
-	db, err = sql.Open("postgres", cfg)
+	db, err := sql.Open("postgres", cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	pingDatabase()
+	pingDatabase(db)
 	fmt.Println("Connected!")
 }
 
-func pingDatabase() {
+func pingDatabase(db *sql.DB) {
 	pingErr := db.Ping()
 	if pingErr != nil {
 		log.Fatal(pingErr)
@@ -38,13 +44,14 @@ func pingDatabase() {
 }
 
 func createWebService() {
-	engine := gin.New()
-	engine.Use(gin.Recovery())
+	engine := gin.Default()
+	engine.SetTrustedProxies(nil)
 	engine = defineRoutes(engine)
-	engine.Run(fmt.Sprintf(":%s", getEnvVariable("PORT")))
+	engine.Run(getEnvVariable("PORT"))
 }
 
 func defineRoutes(engine *gin.Engine) *gin.Engine {
+	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	engine.GET("/")
 	return engine
 }
