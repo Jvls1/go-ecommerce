a98719b6969416fbf537d531c5a21c162c23c84b
Joao <joaovlopes000@gmail.com>
2024-02-29 16:28:58 -0300

    Closes #33

---
+++ auth-service/db.sql
@@ -0,0 +1,52 @@
+CREATE TABLE users
+(
+    id         UUID                  DEFAULT gen_random_uuid(),
+    username   VARCHAR(255) NOT NULL UNIQUE,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP             DEFAULT now(),
+    deleted_at TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles
+(
+    id          UUID                  DEFAULT gen_random_uuid(),
+    name        VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT,
+    created_at  TIMESTAMP    NOT NULL DEFAULT now(),
+    updated_at  TIMESTAMP             DEFAULT now(),
+    deleted_at  TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE permissions
+(
+    id          UUID                  DEFAULT gen_random_uuid(),
+    name        VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT,
+    created_at  TIMESTAMP    NOT NULL DEFAULT now(),
+    updated_at  TIMESTAMP             DEFAULT now(),
+    deleted_at  TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE user_roles
+(
+    user_id UUID NOT NULL,
+    role_id UUID NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE role_permissions
+(
+    role_id       UUID NOT NULL,
+    permission_id UUID NOT NULL,
+    PRIMARY KEY (role_id, permission_id),
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
+    FOREIGN KEY (permission_id) REFERENCES permissions (id) ON DELETE CASCADE
+);
